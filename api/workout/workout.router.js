ef060aef6b18e464db40895b2220ee34dfe9da32
MuniVenkatesh <muni.pydi@wipro.com>
2017-10-25 15:02:13 +0530

    exercise & workout update

--- api/workout/workout.router.js
+++ api/workout/workout.router.js
@@ -1,7 +1,7 @@
 var workoutController = require('./workout.controller');
 const router = require('express').Router();
 router.post("/create",workoutController.createWorkout);
-router.post("/update",workoutController.editWorkout);
+router.put("/update",workoutController.editWorkout);
 router.get("/:id",workoutController.fetchWorkoutsById);
 router.post("/plan",workoutController.fetchWorkoutsByPlan);
 exports = module.exports = router;
